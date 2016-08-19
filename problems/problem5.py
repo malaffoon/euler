600af516b227ec02d9a6a81f91e3eec9ea8434ac
malaffoon <laffoon@san.rr.com>
2016-08-19 07:56:39 -0700

    learned two new things for a much more elegant solution to problem 5: math.gcd and // operator

--- problems/problem5.py
+++ problems/problem5.py
@@ -7,10 +7,8 @@
    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
-import functools
-import operator
-
-from utils.prime import primegen
+from functools import reduce
+from math import gcd
 
 
 class Problem5(object):
@@ -20,25 +18,9 @@ class Problem5(object):
 
 
 def least_common_multiple(values):
-    """Return least common multiple of given values"""
-    if len(values) == 0: return 0
-    if len(values) == 1: return values[0]
-    if max(values) == 1: return 1
-
-    values = list(values)
-    factors = []
-    for prime in primegen(1+max(values)):
-        if max(values) == 1:
-            break
-        while True:
-            factor = False
-            for (v, value) in enumerate(values):
-                if value % prime == 0:
-                    values[v] = int(value/prime)
-                    factor = True
-            if factor: factors.append(prime)
-            else: break
-    return functools.reduce(operator.mul, factors, 1)
+    def lcm(a, b):
+        return (a * b) // gcd(a, b)
+    return reduce(lcm, values, 1)
 
 
 if __name__ == '__main__':
