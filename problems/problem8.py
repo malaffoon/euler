5235a0eef1d7f69dcca3108fb9986bd5bd120040
malaffoon <laffoon@san.rr.com>
2016-09-23 09:13:33 -0700

    problem 8, improve performance by skipping 0's, and use map()

--- problems/problem8.py
+++ problems/problem8.py
@@ -10,17 +10,40 @@ Find the thirteen adjacent digits in the 1000-digit number that have the greates
 What is the value of this product?
 """
 
-import functools
+from functools import reduce
+import operator
 
 PROBLEM8_DATA = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
 
 
 class Problem8(object):
     @staticmethod
-    def solve(n=13):
+    def solve0(n=13):
+        # ignore 0's: 1.7ms
         digits = [int(s) for s in PROBLEM8_DATA]
-        return max(functools.reduce(lambda x, y: x * y, seg, 1) for seg in
-                   (digits[i:i + n] for i in range(0, len(digits) - n)))
+        return max(reduce(operator.mul, seg, 1) for seg in
+                   map(lambda i: digits[i:i + n], range(0, len(digits) - n)))
+
+    @staticmethod
+    def solve(n=13):
+        # advance slowly past 0's: 1.0ms
+        digits = [int(s) for s in PROBLEM8_DATA]
+        return max(reduce(operator.mul, seg, 1) for seg in
+                   map(lambda i: digits[i:i + n], range(0, len(digits) - n)) if 0 not in seg)
+
+    @staticmethod
+    def solve3(n=13):
+        # advance aggressively past 0's: 0.90ms
+        digits = [int(s) for s in PROBLEM8_DATA]
+        i = 0
+        result = 0
+        while i + n < len(digits):
+            if digits[i + n - 1] == 0:
+                i += n
+                continue
+            result = max(result, reduce(operator.mul, digits[i:i + n], 1))
+            i += 1
+        return result
 
 
 if __name__ == '__main__':
--- tests/test_problem8.py
+++ tests/test_problem8.py
@@ -6,9 +6,13 @@ from problems.problem8 import Problem8
 class Problem8Tests(unittest.TestCase):
     def test_solve(self):
         self.assertEquals(23514624000, Problem8.solve())
+        self.assertEquals(23514624000, Problem8.solve0())
+        self.assertEquals(23514624000, Problem8.solve3())
 
     def test_example(self):
         self.assertEqual(5832, Problem8().solve(4))
+        self.assertEqual(5832, Problem8().solve0(4))
+        self.assertEqual(5832, Problem8().solve3(4))
 
 
 if __name__ == '__main__':
