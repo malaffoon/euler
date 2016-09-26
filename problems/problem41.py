020efc7b68da2bd6b10d85701d2b3ff76ea33622
malaffoon <laffoon@san.rr.com>
2016-09-26 08:26:30 -0700

    problem 41 tweak

--- problems/problem41.py
+++ problems/problem41.py
@@ -6,7 +6,13 @@ We shall say that an n-digit number is pandigital if it makes use of all the dig
 For example, 2143 is a 4-digit pandigital and is also prime.
 
 What is the largest n-digit pandigital prime that exists?
+
+Optimization:
+Pandigital numbers of length 2, 3, 5, 6, 8, and 9 will always be divisible by 3.
+So skip up to 1,000,000, and stop at 10,000,000.
 """
+
+from itertools import dropwhile
 
 from utils.mathex import is_pandigital
 from utils.prime import generator
@@ -15,9 +21,7 @@ from utils.prime import generator
 class Problem41(object):
     @staticmethod
     def solve():
-        # NOTE: this isn't really a robust solution since i limit it to seven digits.
-        # More than that took too long to solve, so i lucked out this was an okay limit.
-        return max(p for p in generator(10000000) if is_pandigital(p))
+        return max(p for p in dropwhile(lambda x: x < 1000000, generator(10000000)) if is_pandigital(p))
 
 
 if __name__ == '__main__':
