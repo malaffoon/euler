18bd5e98f97b7eeac27d95d090498dd58ab6fe01
malaffoon <laffoon@san.rr.com>
2016-10-09 08:38:46 -0700

    problem 92 cleanup; fiddle with permutation approach

--- problems/problem92.py
+++ problems/problem92.py
@@ -26,35 +26,56 @@ chain once, then lookup answer. Count 'em. Took 13s.
 The majority of the execution time is the 10million calls to chain(). We should be able to eliminate calculating
 combinations of numbers but that seems more work than the simple chain call.
 """
+import itertools as it
+import math
+
 
 class Problem92(object):
-    @staticmethod
-    def solve():
-        def chain(n):
-            result = 0
-            while n:
-                d = n % 10
-                result += d*d
-                n //= 10
-            return result
 
+    @staticmethod
+    def solve(limit=10000000):
+        lookup = Problem92.__lookup_table(limit)
+        return sum(1 for c in (Problem92.__next_in_chain(i) for i in range(1, limit)) if lookup[c] == 89)
+
+    @staticmethod
+    def __next_in_chain(n):
+        result = 0
+        while n:
+            d = n % 10
+            result += d*d
+            n //= 10
+        return result
+
+    @staticmethod
+    def __lookup_table(limit):
         # fully calculate lookup table
-        lookup = [0] * 568
+        lookup_limit = 1 + int(math.log10(limit)) * 9 * 9
+        lookup = [0] * lookup_limit
         lookup[1] = 1
         lookup[89] = 89
-        for i in range(2, 568):
+        for i in range(2, lookup_limit):
             n = i
-            while True:
-                if n == 1 or lookup[n] == 1:
-                    lookup[i] = 1
-                    break
-                if n == 89 or lookup[n] == 89:
-                    lookup[i] = 89
-                    break
-                n = chain(n)
+            while not lookup[n]:
+                n = Problem92.__next_in_chain(n)
+            lookup[i] = lookup[n]
+        return lookup
 
-        # now, count the ones that hit 89
-        return sum(1 for c in (chain(i) for i in range(1, 10000000)) if lookup[c] == 89)
+    # TODO - what about the combinatorial approach?
+    # TODO   i'm having trouble counting permutations with leading zeros which have to be discounted
+    @staticmethod
+    def solve_comb():
+        def calc_permutations(s):
+            # len(s)! / dup-count1! / dup-count2! - leading-zero-permutations
+            return 1
+
+        lookup = Problem92.__lookup_table(10000000)
+        total = sum(1 for c in range(1, 100) if lookup[c] == 89)
+        for l in range(3, 8):
+            for c in (''.join(t) for t in it.combinations_with_replacement('9876543210', l)):
+                if lookup[Problem92.__next_in_chain(int(c))] == 89:
+                    total += calc_permutations(c)
+                    pass
+        return total
 
 
 if __name__ == '__main__':
